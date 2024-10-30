5c42cbf84b726e25ce3ff0e92e4646e9bc600104
Zakhar Derzhko bc2024-5 <zakhar.derzhko@lnu.edu.ua>
2024-10-30 22:59:44 +0200

    Виконання другої частини роботи - додано методи GET /notes/, PUT /notes/, DELETE /notes/

--- index.js
+++ index.js
@@ -30,3 +30,36 @@ server.listen(option.port, option.host, () => {
     console.log(`Server is running on http://${option.host}:${option.port}`);
 });
 
+app.get('/notes/:noteName', (req, res) => {
+    const notePath = path.join(option.cache, req.params.noteName);
+
+    if (!fs.existsSync(notePath)) {
+        return res.status(404).send('Not found');
+    }
+
+    const noteText = fs.readFileSync(notePath, 'utf8');
+    res.send(noteText);
+});
+
+app.put('/notes/:noteName', (req, res) => {
+    const notePath = path.join(option.cache, req.params.noteName);
+
+    if (!fs.existsSync(notePath)) {
+        return res.status(404).send('Not found');
+    }
+
+    const newText = req.body.text || '';
+    fs.writeFileSync(notePath, newText);
+    res.send('Note updated');
+});
+
+app.delete('/notes/:noteName', (req, res) => {
+    const notePath = path.join(option.cache, req.params.noteName);
+
+    if (!fs.existsSync(notePath)) {
+        return res.status(404).send('Not found');
+    }
+
+    fs.unlinkSync(notePath);
+    res.send('Note deleted');
+});
